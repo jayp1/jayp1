561d68d68117f02fe6181096a19be84e2be0b718
Jarryd Palzenberger <jpalzenberger@squidcard.local>
2020-06-04 17:28:56 +0100

    Updates

---
+++ precache-manifest.3f5d23da98d96f670ae7df2e323ca49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c089cfd43d31eed75b6e0cbb88de83d",
+    "url": "/jayp1/index.html"
+  },
+  {
+    "revision": "4bfa58b0c5ba0f8eaf08",
+    "url": "/jayp1/static/css/main.299c8e33.chunk.css"
+  },
+  {
+    "revision": "078d768971e46eba6444",
+    "url": "/jayp1/static/js/2.feb070be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jayp1/static/js/2.feb070be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfa58b0c5ba0f8eaf08",
+    "url": "/jayp1/static/js/main.52723b26.chunk.js"
+  },
+  {
+    "revision": "a345aca14fdf90c36dc6",
+    "url": "/jayp1/static/js/runtime-main.e4191026.js"
+  }
+]);
